f5fcc8c64ee0add616a5c4b9dfd3fb3bd66ac74a
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-01-07 15:11:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-
-<?php
-   echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta property="og: title" content="WebniJoseph">
+    <meta name="author" content="Joseph Morales">
+    <title> First PHP</title>
+</head>
+<body>
+   <?php
+        echo "Hello World";
+   ?>
+</body>
+</html>
